6e8565adfc6372d536c592b0cc24e9677f5e79cf
s27238 <s27238@pjwstk.edu.pl>
2024-03-12 13:30:59 +0100

    Add average method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfication 1");
 Console.WriteLine("Modyfication 2");
 Console.WriteLine("Modyfication 3");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double) sum / numbers.Length;
+}
