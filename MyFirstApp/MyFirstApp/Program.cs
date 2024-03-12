de73026864b8177e98df996c09cf06816190b884
s27238 <s27238@pjwstk.edu.pl>
2024-03-12 13:32:28 +0100

    Print average on the console

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -14,5 +14,9 @@ static double GetAverage(int[] numbers)
         sum += number;
     }
 
-    return (double) sum / numbers.Length;
+    return (double)sum / numbers.Length;
 }
+
+int[] numbers = { 1, 2, 3, 4 };
+double average = GetAverage(numbers);
+Console.WriteLine(average);
